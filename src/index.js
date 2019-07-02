e41aeeccd64ac4faab0b63f0c8b2d29486b0a4c4
everlive <raybeez@gmail.com>
2019-07-02 14:07:55 +0300

    creating the api

--- src/index.js
+++ src/index.js
@@ -11,8 +11,39 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+  });
+  
+  app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+  
+  app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+  });
+  
+  app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+  });
+
+  app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user resource');
+  });
+  
+  app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user resource');
+  });
+  
+  app.put('/users/:userId', (req, res) => {
+    return res.send(`PUT HTTP method on user/${req.params.userId} resource`,);
+  });
+  
+  app.delete('/users/:userId', (req, res) => {
+    return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+  });
 app.listen(process.env.PORT, () =>
-console.log('hello node from ${process.env.PORT}'),
+console.log(`hello node from ${process.env.PORT}`),
 );
 
 console.log(process.env.MY_SECRET);
