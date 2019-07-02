308283b2976622915d4d0fa6f0491c2200186a7d
everlive <raybeez@gmail.com>
2019-07-02 14:22:30 +0300

    json and body parsing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import express from 'express';
-
+import uuid from 'uuid';
 
 const app = express();
 
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
@@ -28,9 +30,32 @@ app.get('/', (req, res) => {
   });
 
   app.get('/users', (req, res) => {
-    return res.send('GET HTTP method on user resource');
+    return res.send(Object.values(users));
   });
   
+  app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+  });
+
+  app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+  });
+  
+  app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId]);
+  });
+  app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+      id,
+      text: req.body.text,
+    };
+  
+    messages[id] = message;
+  
+    return res.send(message);
+  });
+
   app.post('/users', (req, res) => {
     return res.send('POST HTTP method on user resource');
   });
@@ -42,6 +67,30 @@ app.get('/', (req, res) => {
   app.delete('/users/:userId', (req, res) => {
     return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
   });
+
+  let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+  };
+  
+  let messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+    },
+  };
 app.listen(process.env.PORT, () =>
 console.log(`hello node from ${process.env.PORT}`),
 );
